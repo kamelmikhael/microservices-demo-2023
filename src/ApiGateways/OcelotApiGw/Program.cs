28288ccd24463bc1e2cc4b7adaf372415551bcc0
Kamel Danial <Kamel.Danial@keitmea.com>
2023-10-02 18:30:19 +0300

    Finish api gateway

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,14 +1,18 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
 //Configure Ocelot Json files here
-builder.Configuration.AddJsonFile(
-    $"ocelot.{builder.Environment.EnvironmentName}.json", 
-    optional: true, 
-    reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration);
+builder.Configuration
+    .AddJsonFile(
+        $"ocelot.{builder.Environment.EnvironmentName}.json", 
+        optional: true, 
+        reloadOnChange: true);
+builder.Services
+    .AddOcelot(builder.Configuration)
+    .AddCacheManager(x => x.WithDictionaryHandle());
 
 var app = builder.Build();
 
